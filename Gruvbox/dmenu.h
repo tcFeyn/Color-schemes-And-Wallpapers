06a28ad046a3d72f566b55bb785cd5787ea9c888
David Ferreira <65172017+tcFeyn@users.noreply.github.com>
2021-05-25 20:05:55 +0100

    Added Gruvbox color-scheme

---
+++ Gruvbox/dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*                  fg         bg       */
+	[SchemeNorm] = { "#c8ccd4", "#282c34" },
+	[SchemeSel] = { "#282828", "#98971a" },
+	[SchemeOut] = { "#c8ccd4", "#c8ccd4" },
+};
